45820bf15116e978271d0485f816dccb8cba9791
Vaibhav Pandey <contact@vaibhavpandey.com>
2025-12-26 17:39:23 +0530

    Do not use sudo as needed

--- iso/config/hooks/post/05-plymouth-theme.sh
+++ iso/config/hooks/post/05-plymouth-theme.sh
@@ -3,8 +3,8 @@ set -e
 
 echo "[plymouth-theme] Setting default theme and regenerating initramfs"
 
-# Set the default Plymouth theme
-sudo update-alternatives --install \
+# Set the default Plymouth theme using update-alternatives
+update-alternatives --install \
   /usr/share/plymouth/themes/text.plymouth \
   text.plymouth \
   /usr/share/plymouth/themes/tejas/tejas.plymouth \
@@ -16,6 +16,6 @@ update-alternatives --set \
 
 # Regenerate initramfs for all installed kernels to include the theme
 # This ensures the live system uses the custom theme
-sudo update-initramfs -u -k all
+update-initramfs -u -k all
 
 echo "[plymouth-theme] Theme configured and initramfs regenerated"
