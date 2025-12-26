33c8672b99bc37bd30d7d8c02c595449ceb2ddff
Vaibhav Pandey <contact@vaibhavpandey.com>
2025-12-26 20:23:53 +0530

    Run locale-gen before update-locale

--- iso/config/hooks/post/01-set-default-locale.sh
+++ iso/config/hooks/post/01-set-default-locale.sh
@@ -3,4 +3,11 @@ set -e
 
 echo "[set-default-locale] Setting default locale to en_IN"
 
+# Add en_IN.UTF-8 to locale.gen
+echo "en_IN.UTF-8 UTF-8" >> /etc/locale.gen
+
+# Generate locales
+locale-gen
+
+# Set default locale to en_IN
 update-locale LANG=en_IN.UTF-8
